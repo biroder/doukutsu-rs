5122adeb14527cec9a20b0e39806df5c41cba2d7
biroder <107300789+biroder@users.noreply.github.com>
2025-01-01 00:43:28 +0200

    misc

---
+++ misc/gradle_pkg_version.sh
@@ -0,0 +1,18 @@
+function get_len {
+    echo $(echo $1 | wc -c)
+}
+
+function get_version {
+    local quote_len=0
+    if [ "$3" = "1" ]; then
+        quote_len=1
+    fi
+
+    local prop_len=$(get_len $1)
+    local line="$(grep $1 $2 | tr -d [:space:])"
+    local line_len=$(get_len $line)
+    local version="$(echo $line | cut -c $((prop_len+quote_len))-$((line_len-2*quote_len)))"
+    echo $version
+}
+
+get_version $1 $2 $3
